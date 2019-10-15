cdda59c6635a4d07a6dc02bb2e1d5a0b375c828a
Christopher Dengsø <chrdengso@gmail.com>
2019-10-15 14:29:08 +0800

    genres selector

--- src/shared/components/common/ToggleButtonInput.js
+++ src/shared/components/common/ToggleButtonInput.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { SecondaryButton } from 'components/Blocks';
+import Checkmark from '../../assets/Checkmark';
 
 class ToggleButton extends Component {
     static defaultProps = {
@@ -6,49 +8,25 @@ class ToggleButton extends Component {
         label: 'ToggleButton',
     };
 
-    constructor(props) {
-        super(props);
-        this.selfRef = React.createRef();
-        this.state = {
-            toggled: props.active,
-            value: '',
-        };
-    }
-
-    componentDidMount() {
-        this.selfRef.current.focus();
-        this.selfRef.current.addEventListener('keypress', (e) => {
-            const key = e.which || e.keyCode;
-            if (key === 13) {
-                // 13 is enter
-                !!this.props.onChange && this.props.onChange(this.state.value);
-            }
-        });
-    }
-
-    // UNSAFE_componentWillReceiveProps(nextProps, nextContext) {
-    //     if (nextProps.active !== undefined) {
-    //         this.setState({
-    //             toggled: nextProps.active,
-    //         });
-    //     }
-    // }
-
     onChange = (e) => {
-        this.setState({ value: e.target.value });
         !!this.props.onChange && this.props.onChange(e.target.value);
     };
 
     render() {
         return (
-            <button
-                {...this.props}
-                className="edit-text-button"
-                active={this.state.toggled}
-                onClick={null}
-            >
-                <input onChange={this.onChange} ref={this.selfRef} type="text" />
-            </button>
+            <SecondaryButton {...this.props} className="edit-text-button" onClick={null}>
+                <input onChange={this.onChange} type="text" />
+                <Checkmark
+                    style={{
+                        right: '6px',
+                        top: '13px',
+                        width: '15px',
+                        height: '15px',
+                        position: 'absolute',
+                    }}
+                    color="#50e3c2"
+                />
+            </SecondaryButton>
         );
     }
 }
