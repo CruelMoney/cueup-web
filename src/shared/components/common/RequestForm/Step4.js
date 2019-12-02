bd5927c51ca0c2baf6acc03065d29202c01fe7c6
Christopher Dengsø <chrdengso@gmail.com>
2019-12-02 09:46:11 +0100

    confirm email

--- src/shared/components/common/RequestForm/Step4.js
+++ src/shared/components/common/RequestForm/Step4.js
@@ -2,7 +2,7 @@ import React from 'react';
 import emailValidator from 'email-validator';
 import { Row, TeritaryButton, PrimaryButton, SmartButton } from 'components/Blocks';
 import { BodySmall } from 'components/Text';
-import { Input } from 'components/FormComponents';
+import { Input, InputRow } from 'components/FormComponents';
 import addTranslate from 'components/higher-order/addTranslate';
 import { RequestSection } from './RequestForm';
 
@@ -66,25 +66,46 @@ const Step4 = ({
                     </Input>
                 </RequestSection>
                 <RequestSection>
-                    <Input
-                        type="email"
-                        name="contactEmail"
-                        label={translate('request-form.step-4.contact-email')}
-                        autoComplete="email"
-                        placeholder="Email"
-                        defaultValue={form.contactEmail}
-                        validation={(v) =>
-                            emailValidator.validate(v) ? null : 'Not a valid email'
-                        }
-                        onSave={(contactEmail) => handleChange({ contactEmail })}
-                        registerValidation={registerValidation('contactEmail')}
-                        unregisterValidation={unregisterValidation('contactEmail')}
-                    >
-                        <BodySmall>
-                            {translate('request-form.step-4.contact-email-description')}
-                        </BodySmall>
-                    </Input>
+                    <InputRow small>
+                        <Input
+                            half
+                            type="email"
+                            name="contactEmail"
+                            label={translate('request-form.step-4.contact-email')}
+                            autoComplete="email"
+                            placeholder="Email"
+                            defaultValue={form.contactEmail}
+                            validation={(v) =>
+                                emailValidator.validate(v) ? null : 'Not a valid email'
+                            }
+                            onSave={(contactEmail) => handleChange({ contactEmail })}
+                            registerValidation={registerValidation('contactEmail')}
+                            unregisterValidation={unregisterValidation('contactEmail')}
+                        />
+                        <Input
+                            half
+                            type="email"
+                            name="contactEmailConfirm"
+                            label="Confirm"
+                            autoComplete="email"
+                            placeholder="Confirm Email"
+                            defaultValue={form.contactEmailConfirm}
+                            validation={(v) => {
+                                if (v?.toUpperCase() !== form.contactEmail?.toUpperCase()) {
+                                    return 'Emails not matching';
+                                }
+                                return null;
+                            }}
+                            onSave={(contactEmailConfirm) => handleChange({ contactEmailConfirm })}
+                            registerValidation={registerValidation('contactEmailConfirm')}
+                            unregisterValidation={unregisterValidation('contactEmailConfirm')}
+                        />
+                    </InputRow>
+                    <BodySmall>
+                        {translate('request-form.step-4.contact-email-description')}
+                    </BodySmall>
                 </RequestSection>
+
                 <Row right style={{ marginTop: '12px' }}>
                     <TeritaryButton type="button" className="back-button" onClick={back}>
                         {translate('back')}
