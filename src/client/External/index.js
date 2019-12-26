438c1c6e388c836a8c788d0fdfe57f46bf62c16b
Christopher <chrdengso@gmail.com>
2019-12-26 18:30:39 +0100

    defer stripe

--- src/client/External/index.js
+++ src/client/External/index.js
@@ -2,11 +2,13 @@ import loadable from '@loadable/component';
 import { useEffect } from 'react';
 
 const Olark = loadable(() => import('./olark'));
+const Stripe = loadable(() => import('./stripe'));
 
 const useExternals = () => {
     useEffect(() => {
         setTimeout(() => {
             Olark.preload();
+            Stripe.preload();
         }, 4000);
     }, []);
 };
