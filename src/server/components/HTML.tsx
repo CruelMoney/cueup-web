38b88a239adbcc59d178e94f6815d600c1cbf5e7
Christopher <chrdengso@gmail.com>
2019-12-26 17:17:49 +0100

    added olark

--- src/server/components/HTML.tsx
+++ src/server/components/HTML.tsx
@@ -88,6 +88,20 @@ const HTML = ({
             <div id="tooltip-portal" />
             <div id="mobile-menu-portal" />
             <div id="notification-portal" />
+
+            <script
+                async
+                // eslint-disable-next-line react/no-danger
+                type="text/javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    ;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
+                  
+                    /* custom configuration goes here (www.olark.com/documentation) */
+                    olark.identify('8095-537-10-2169');
+                `,
+                }}
+            />
         </body>
     </html>
 );
