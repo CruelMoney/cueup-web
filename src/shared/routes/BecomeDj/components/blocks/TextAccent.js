41c2faf146b896567b03dc238f0d5d71888fbd00
Laurent Lahmy <lahmy.laurent@gmail.com>
2020-01-10 16:02:57 +0800

    component structure with blocks directory

---
+++ src/shared/routes/BecomeDj/components/blocks/TextAccent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export const TextAccent = styled.h3`
+    color: #00d1ff;
+    text-align: center;
+    font-size: 15px;
+    ${(margin) => (margin ? margin : '0')}
+`;
