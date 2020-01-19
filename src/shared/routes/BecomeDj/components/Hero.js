f5ec43bc82b360043965efb16af8c7135c7ad403
Christopher <chrdengso@gmail.com>
2020-01-19 19:07:16 +0100

    button glow

--- src/shared/routes/BecomeDj/components/Hero.js
+++ src/shared/routes/BecomeDj/components/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Container, Col, Row } from 'components/Blocks';
+import { NavLink as Link } from 'react-router-dom';
+import { Container, Col, Row, PrimaryButton } from 'components/Blocks';
 import addTranslate from '../../../components/higher-order/addTranslate';
 import content from '../content.json';
 import { TextAccent } from '../components/blocks/TextAccent';
 import { Title } from '../components/blocks/Title';
 import { SubTitle } from '../components/blocks/SubTitle';
-import ButtonLinkGlow from './blocks/ButtonLinkGlow';
 
 const OrderedContainer = styled(Container)`
     order: 1;
@@ -18,12 +18,20 @@ const HeroCol = styled(Col)`
     width: 550px;
 `;
 
-const HeroButtonLink = styled(ButtonLinkGlow)`
-    display: block;
-    margin-top: 40px;
-    font-size: 18px;
+const HeroButtonLink = styled(PrimaryButton)`
     height: 50px;
-    box-shadow: 0px 0px 30px 30px #31daff;
+    line-height: 50px;
+    border-radius: 13px;
+    font-size: 18px;
+    box-shadow: 0 0 45px #00d1ff;
+    margin: 40px auto 0 auto;
+    text-align: center;
+    max-width: 250px;
+    transition-duration: 750ms;
+    :hover {
+        box-shadow: 0 0 70px #00d1ff;
+        transform: scale(1.02);
+    }
 `;
 
 const Hero = (props) => {
@@ -37,7 +45,7 @@ const Hero = (props) => {
                     </TextAccent>
                     <Title>{translate('become-dj.hero.the-only-profile-a-DJ-needs')}</Title>
                     <SubTitle white>{translate('become-dj.hero.subtitle')}</SubTitle>
-                    <HeroButtonLink color="#00D1FF" to={props.firstTo} className="button elevated">
+                    <HeroButtonLink color="#00D1FF" to={props.firstTo}>
                         {translate('become-dj.hero.apply-to-become-dj')}
                     </HeroButtonLink>
                 </HeroCol>
