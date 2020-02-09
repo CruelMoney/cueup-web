1ef538019fb048b5606a21f232b992724a9c63df
Christopher <chrdengso@gmail.com>
2020-02-09 19:47:19 +0800

    fixed cookie bug

--- src/shared/utils/AuthService.js
+++ src/shared/utils/AuthService.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 
 class AuthService extends EventEmitter {
     setSession(token) {
+        console.log('set session');
         // Set the time that the access token will expire at
         localStorage.setItem('token', token);
         const expire = moment()
@@ -12,6 +13,8 @@ class AuthService extends EventEmitter {
     }
 
     logout() {
+        console.log('log out');
+
         // Clear access token and ID token from local storage
         localStorage.removeItem('token');
         document.cookie = 'x-token=; path=/; expires = Thu, 01 Jan 1970 00:00:00 GMT';
@@ -25,12 +28,28 @@ class AuthService extends EventEmitter {
 
     getAccessToken() {
         // Retrieves the user token from localStorage
-        return localStorage.getItem('token');
+        const token = getCookie('x-token') ?? localStorage.getItem('token');
+        console.log({ token });
+        return token;
     }
 
     getToken() {
         return this.getAccessToken();
     }
+}
+
+function getCookie(name) {
+    const nameEQ = name + '=';
+    const ca = document.cookie.split(';');
+    for (let c of ca) {
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
 }
 
 export const authService = new AuthService();
