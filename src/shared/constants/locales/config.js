81dc5b0299148c9ada990897b2115161b7e30d90
Christopher <chrdengso@gmail.com>
2020-03-30 14:57:41 +0800

    soo long so good

--- src/shared/constants/locales/config.js
+++ src/shared/constants/locales/config.js
@@ -1,12 +1,18 @@
+import { languagesArray } from './languages';
+
 const config = {
     debug: true,
+    language: 'en',
     fallbackLng: 'en',
+    languages: languagesArray,
     cleanCode: true,
     load: 'languageOnly',
     keySeparator: false, // we do not use keys in form messages.welcome
-
     interpolation: {
         escapeValue: false, // react already safes from xss
+    },
+    detection: {
+        order: ['path', 'session'],
     },
 };
 
