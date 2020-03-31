31348e96267a85c1c9ceb51006550d0b9feac7eb
Christopher <chrdengso@gmail.com>
2020-03-31 19:45:51 +0800

    save

--- src/shared/components/FormComponents.js
+++ src/shared/components/FormComponents.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import styled, { css } from 'styled-components';
 import Checkmark from '../assets/Checkmark';
 import {
