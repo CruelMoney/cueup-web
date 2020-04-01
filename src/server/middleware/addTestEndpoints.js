d904ce03b904d93a64cd80df0bb6a51badf93de8
Christopher <chrdengso@gmail.com>
2020-04-02 01:27:01 +0800

    change oring

--- src/server/middleware/addTestEndpoints.js
+++ src/server/middleware/addTestEndpoints.js
@@ -1,10 +1,8 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import express from 'express';
 
 const API_URL = process.env.REACT_APP_CUEUP_GQL_DOMAIN;
 const target = API_URL;
-console.log({ target });
-const apiProxy = createProxyMiddleware({ target });
+const apiProxy = createProxyMiddleware({ target, changeOrigin: true });
 
 const addTestEndpoints = (app) => {
     app.use('/test', apiProxy);
