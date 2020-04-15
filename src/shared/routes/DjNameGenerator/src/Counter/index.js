e89e309f020ba0b5061ab0b55690a6ea99de7d75
Christopher <chrdengso@gmail.com>
2020-04-15 11:33:37 +0800

    throttle render of count

--- src/shared/routes/DjNameGenerator/src/Counter/index.js
+++ src/shared/routes/DjNameGenerator/src/Counter/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { useSubscription, useQuery } from 'react-apollo';
+import throttle from 'react-throttle-render';
 import { COUNT_UPDATED_SUB, GET_GENERATED_NAMES_COUNT } from '../gql';
 
-const Counter = () => {
+const Counter = ({ count }) => {
+    return (
+        <p className="counter">
+            <span>{count}</span> DJ names generated
+        </p>
+    );
+};
+
+const DebouncedCounter = throttle(500)(Counter);
+
+const DataWrapper = () => {
     const { data: countData } = useQuery(GET_GENERATED_NAMES_COUNT, { ssr: false });
 
     const { data } = useSubscription(COUNT_UPDATED_SUB);
@@ -13,11 +24,7 @@ const Counter = () => {
         return null;
     }
 
-    return (
-        <p className="counter">
-            <span>{count}</span> DJ names generated
-        </p>
-    );
+    return <DebouncedCounter count={count} />;
 };
 
-export default Counter;
+export default DataWrapper;
