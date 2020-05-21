bfb4485f165514860868500d14f0728b23ae4619
Christopher <chrdengso@gmail.com>
2020-05-21 23:12:00 +0800

    code splitting

--- babel.config.js
+++ babel.config.js
@@ -12,11 +12,11 @@ module.exports = (api) => {
                 '@babel/env',
                 {
                     modules: false,
-                    useBuiltIns: 'usage',
-                    corejs: {
-                        version: 3,
-                        proposals: true,
-                    },
+                    useBuiltIns: false,
+                    // corejs: {
+                    //     version: 3,
+                    //     proposals: true,
+                    // },
                     ...(isTargetWeb === false && {
                         targets: {
                             node: 'current',
@@ -34,7 +34,7 @@ module.exports = (api) => {
             '@babel/proposal-optional-chaining',
             '@babel/plugin-proposal-nullish-coalescing-operator',
             '@babel/syntax-dynamic-import',
-            // '@babel/plugin-transform-runtime',
+            '@babel/plugin-transform-runtime',
             'babel-plugin-styled-components',
             'macros',
             isDev && isTargetWeb && 'react-refresh/babel',
--- config/webpack.config.ts/client.base.ts
+++ config/webpack.config.ts/client.base.ts
@@ -85,15 +85,28 @@ export default {
         ],
         namedModules: true,
         noEmitOnErrors: true,
+        // Automatically split vendor and commons
+        // https://twitter.com/wSokra/status/969633336732905474
+        // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
         splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    chunks: 'all',
-                },
-            },
+            chunks: 'all',
+            name: false,
         },
+        // Keep the runtime chunk separated to enable long term cagitching
+        // https://twitter.com/wSokra/status/969679223278505985
+        // https://github.com/facebook/create-react-app/issues/5358
+        runtimeChunk: {
+            name: (entrypoint) => `runtime-${entrypoint.name}`,
+        },
+        // splitChunks: {
+        //     cacheGroups: {
+        //         commons: {
+        //             test: /[\\/]node_modules[\\/]/,
+        //             name: 'vendor',
+        //             chunks: 'all',
+        //         },
+        //     },
+        // },
     },
     stats: {
         cached: false,
