dbc0710a736abc76d28700b9e2a971ddf1abf292
Christopher <chrdengso@gmail.com>
2020-05-21 21:35:55 +0800

    updated tooling

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,57 @@
-module.exports = {
-    compact: true,
-    presets: [
-        [
-            '@babel/env',
-            {
-                modules: false,
-                targets: {
-                    node: 'current',
+module.exports = (api) => {
+    api.cache.using(() => process.env.NODE_ENV);
+
+    const isTargetWeb = api.caller((caller) => caller && caller.target === 'web');
+
+    const isDev = ['test', 'production'].includes(process.env.NODE_ENV) === false;
+
+    return {
+        compact: true,
+        presets: [
+            [
+                '@babel/env',
+                {
+                    modules: false,
+                    useBuiltIns: 'entry',
+                    corejs: 3.6,
+                    ...(isTargetWeb === false && {
+                        targets: {
+                            node: 'current',
+                        },
+                    }),
                 },
-            },
-        ],
-        '@babel/react',
-        '@babel/typescript',
-    ],
-    plugins: [
-        '@loadable/babel-plugin',
-        '@babel/proposal-object-rest-spread',
-        '@babel/proposal-class-properties',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/syntax-dynamic-import',
-        'macros',
-        'babel-plugin-styled-components',
-    ],
-    env: {
-        test: {
-            plugins: [
-                '@loadable/babel-plugin',
-                '@babel/transform-modules-commonjs',
-                '@babel/syntax-dynamic-import',
-                '@babel/plugin-transform-runtime',
-                'babel-plugin-styled-components',
             ],
+            '@babel/react',
+            '@babel/typescript',
+        ],
+        plugins: [
+            '@babel/proposal-object-rest-spread',
+            '@babel/proposal-class-properties',
+            '@babel/proposal-optional-chaining',
+            '@babel/syntax-dynamic-import',
+            'babel-plugin-styled-components',
+            'macros',
+            isDev && isTargetWeb && 'react-refresh/babel',
+        ].filter(Boolean),
+        env: {
+            test: {
+                plugins: [
+                    '@babel/transform-modules-commonjs',
+                    '@babel/syntax-dynamic-import',
+                    '@babel/plugin-transform-runtime',
+                ],
+            },
+            tooling: {
+                presets: [
+                    [
+                        '@babel/env',
+                        {
+                            modules: 'commonjs',
+                        },
+                    ],
+                    '@babel/typescript',
+                ],
+            },
         },
-    },
+    };
 };
