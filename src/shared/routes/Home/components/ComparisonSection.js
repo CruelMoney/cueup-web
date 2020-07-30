056344f58109b18c2dfff70ab4a6dd96f5578ad0
Christopher <chrdengso@gmail.com>
2020-07-30 21:58:39 +0800

    comaprisson

--- src/shared/routes/Home/components/ComparisonSection.js
+++ src/shared/routes/Home/components/ComparisonSection.js
@@ -2,8 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import { InlineIcon } from '@iconify/react';
 import checkmarkCircle from '@iconify/icons-ion/checkmark-circle';
-import { Container, Col, Row } from 'components/Blocks';
+import { Container, Col, Row, RowWrap } from 'components/Blocks';
 import { H2, TextAccent, Body } from 'components/Text';
+import GracefullImage from 'components/GracefullImage';
+import comparisonImage from '../assets/the_lawn.jpg';
+
+const data = [
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+    { label: 'Finding DJs', traditional: 'Manual', cueup: 'Automatic' },
+];
+
 const ComparisonSection = () => {
     return (
         <Wrapper>
@@ -16,87 +29,42 @@ const ComparisonSection = () => {
                         of finding a good DJ
                     </H2>
                 </Col>
-                <Row>
-                    <Body style={{ maxWidth: 220, marginRight: 60 }}>
+                <RowWrap>
+                    <Body>
                         Cueup is the easiest way for you to get a great DJ for your event. Tell us
                         about your event below, and check out 1000s of qualified DJs.
                     </Body>
                     <ComparisonTable>
-                        <table>
+                        <table cellSpacing={0}>
                             <thead>
-                                <th /> <td>Traditional</td> <td>Cueup</td>
+                                <th /> <th>Traditional</th> <th>Cueup</th>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Finding DJs</th>
-                                    <td>Manual</td>
-                                    <td>
-                                        <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
-                                        Automatic
-                                    </td>
-                                </tr>
+                                {data.map(({ label, traditional, cueup }, idx) => (
+                                    <tr key={idx}>
+                                        <th>{label}</th>
+                                        <td>{traditional}</td>
+                                        <td>
+                                            <InlineIcon icon={checkmarkCircle} color="#00D1FF" />
+                                            {cueup}
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </ComparisonTable>
-                </Row>
+                </RowWrap>
+                <CustomImage src={comparisonImage} />
             </Container>
         </Wrapper>
     );
 };
 
 const ComparisonTable = styled.div`
-    width: 100%;
+    flex: 1;
     word-break: break-word;
     margin: 10px;
-    margin-top: -25px;
+    margin-top: -30px;
     table {
         width: 100%;
         border-collapse: separate;
@@ -104,7 +72,7 @@ const ComparisonTable = styled.div`
     thead {
         th,
         td {
-            padding-bottom: 10px;
+            padding-bottom: 15px;
         }
     }
     tbody {
@@ -121,18 +89,24 @@ const ComparisonTable = styled.div`
         border-radius: 20px;
         padding: 10px;
         background-color: #122b48;
+        box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.4);
     }
     tr {
-        height: 40px;
+        height: 42px;
         position: relative;
     }
     td,
     th {
         padding-left: 10px;
         margin: 0;
+        font-weight: 100;
+        position: relative;
         svg {
             margin-right: 6px;
         }
+    }
+    th {
+        font-weight: 500;
     }
     tr:nth-of-type(2n + 1) {
         th,
@@ -149,12 +123,42 @@ const ComparisonTable = styled.div`
         border-top-right-radius: 10px;
         border-bottom-right-radius: 10px;
     }
+    td:before {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 20%;
+        left: 0;
+        width: 2px;
+        height: 60%;
+        background-color: rgba(255, 255, 255, 0.15);
+    }
 `;
 
 const Wrapper = styled.section`
     display: flex;
     text-align: left;
     padding: 60px 0;
+    ${Body} {
+        max-width: 220px;
+        margin-right: 50px;
+    }
+    @media screen and (max-width: 480px) {
+        ${Body} {
+            max-width: 100%;
+            margin-right: 0;
+        }
+    }
+`;
+
+const CustomImage = styled(GracefullImage)`
+    filter: drop-shadow(0px 4px 30px rgba(0, 0, 0, 0.15));
+    border-radius: 20px;
+    width: 480px;
+    height: 230px;
+    object-fit: cover;
+    margin-top: -70px;
+    z-index: -1;
 `;
 
 export default ComparisonSection;
