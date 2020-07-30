2c7f8f186e44b992dba190de40335a8fd073fa70
Christopher <chrdengso@gmail.com>
2020-07-30 12:48:06 +0800

    improved loading skeleton look

--- src/shared/components/common/LoadingPlaceholder.js
+++ src/shared/components/common/LoadingPlaceholder.js
@@ -5,7 +5,15 @@ class LoadingPlaceholder2 extends Component {
     render() {
         return (
             <div {...this.props}>
-                <Skeleton count={2} height={100} style={{ marginBottom: 9 }} />
+                <Skeleton
+                    count={2}
+                    height={100}
+                    style={{
+                        marginBottom: 9,
+                        backgroundColor: '#eff2f5',
+                        backgroundImage: 'linear-gradient( 90deg,#eff2f5,#f7f7f8,#eff2f5)',
+                    }}
+                />
             </div>
         );
     }
