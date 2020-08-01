b4a323ccaac0f630a1a39cde2486c5d3f6a45a57
Christopher <chrdengso@gmail.com>
2020-08-01 17:23:53 +0800

    new favicon

--- src/shared/index.js
+++ src/shared/index.js
@@ -62,6 +62,14 @@ const Setup = ({ location }) => {
                 <meta name="twitter:description" content={description} />
                 <meta name="twitter:image" content={thumb} />
                 <meta name="twitter:url" content={pageURL} />
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#122b48" />
+                <meta name="msapplication-TileColor" content="#122b48" />
+                <meta name="theme-color" content="#ffffff" />
             </Helmet>
 
             <Switch>
