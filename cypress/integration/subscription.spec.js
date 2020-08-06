a694d7cace412f58ae67eeb778cd513265f53080
Christopher <chrdengso@gmail.com>
2020-08-06 14:51:48 +0200

    updated edit user e2e

--- cypress/integration/subscription.spec.js
+++ cypress/integration/subscription.spec.js
@@ -1,4 +1,11 @@
 /// <reference types="Cypress" />
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+});
+
 describe('Subscription', () => {
     it('Can be paid', () => {
         const user = {
--- cypress/integration/update_user.spec.js
+++ cypress/integration/update_user.spec.js
@@ -1,4 +1,11 @@
 /// <reference types="Cypress" />
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+});
+
 describe('Update User', () => {
     it('Updates permalink', () => {
         const user = {
@@ -6,6 +13,7 @@ describe('Update User', () => {
             password: 't5e3s4t5i8n18g12',
             firstName: 'Christopher',
             lastName: 'Dengsø',
+            genres: [1, 2, 3],
             playingLocation: {
                 name: 'Copenhagen',
                 latitude: 55.6760968,
@@ -21,19 +29,20 @@ describe('Update User', () => {
             cy.setCookie('x-token', response.body.token)
         );
 
+        cy.setCookie('testing', 'true');
+
         cy.getCookie('x-token').should('exist');
         cy.visit('/');
         cy.get('[data-cy=menu-user-link]').click();
-        cy.get('[data-cy=menu-profile-link]').click();
+        cy.get('[data-cy=menu-profile-link]').click({ force: true });
         cy.url().should('include', '/user');
-        cy.get('.card.popup.active *[data-cy=close-popup-button]').click();
         cy.get('[data-cy=navbutton-settings]').click();
         cy.get('input[name=permalink]').clear().type('new-permalink{enter}');
 
         cy.visit('/');
 
         cy.get('[data-cy=menu-user-link]').click();
-        cy.get('[data-cy=menu-profile-link]').click();
+        cy.get('[data-cy=menu-profile-link]').click({ force: true });
         cy.url().should('include', 'new-permalink');
     });
 });
