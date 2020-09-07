29b00e5e58207c395dea7415e391b5fb6219d115
Christopher Dengsø <christopher@cpe.ge-7-0-5-100.vbrnqe10.dk.customer.tdc.net>
2020-09-07 21:55:18 +0200

    tried to fix cls

--- src/shared/routes/DjNameGenerator/src/DjNameGenerator.js
+++ src/shared/routes/DjNameGenerator/src/DjNameGenerator.js
@@ -4,6 +4,9 @@ import { useLocation, Route } from 'react-router';
 import { useMutation } from 'react-apollo';
 import { useSpring, animated } from 'react-spring';
 import { useServerContext } from 'components/hooks/useServerContext';
+import { appRoutes } from 'constants/locales/appRoutes';
+import useTranslate from 'components/hooks/useTranslate';
+import { hideChatButton } from 'utils/supportChat';
 import useGenerateName, { TYPES } from './useGenerateName';
 import DraggableList from './DragableList';
 import ToggleSwitch from './ToggleSwitch';
@@ -48,6 +51,7 @@ const CATEGORIES = [
 ];
 
 function App({ match, history }) {
+    const { translate } = useTranslate();
     const [mutate] = useMutation(NAME_GENERATED);
 
     const [categories, setCategories] = useState([
@@ -201,7 +205,7 @@ function App({ match, history }) {
                 </div>
 
                 <div className="bottom-right">
-                    <a href={'https://cueup.io/become-dj?ref=dj-name-generator'}>
+                    <a href={translate(appRoutes.becomeDj) + '?ref=dj-name-generator'}>
                         <div className="created-by">
                             <h4>Created by</h4>
                             <Logo />
@@ -255,6 +259,10 @@ function useQuery() {
 }
 
 const AppWithMeta = (props) => {
+    useEffect(() => {
+        hideChatButton();
+    }, []);
+
     const params = useQuery();
     const location = useLocation();
     const name = params.get('name');
