fcf2046818a2b74c896514940d64c927a69741c5
Christopher Dengsø <work@cude.io>
2020-09-26 12:39:33 +0200

    test for init

--- src/client/External/oneSignal.js
+++ src/client/External/oneSignal.js
@@ -1,23 +1,25 @@
 export const loadOneSignal = () => {
-    window.OneSignal = window.OneSignal || [];
+    if (!window.OneSignal) {
+        window.OneSignal = window.OneSignal || [];
 
-    window.OneSignal.push(() => {
-        if (window.__ENVIRONMENT__.ONE_SIGNAL_KEY) {
-            window.OneSignal.init({
-                appId: window.__ENVIRONMENT__.ONE_SIGNAL_KEY,
-                autoResubscribe: true,
-                notifyButton: {
-                    enable: false,
-                },
-                welcomeNotification: {
-                    disable: true,
-                },
-                allowLocalhostAsSecureOrigin: true,
-            });
-        }
-    });
+        window.OneSignal.push(() => {
+            if (window.__ENVIRONMENT__.ONE_SIGNAL_KEY) {
+                window.OneSignal.init({
+                    appId: window.__ENVIRONMENT__.ONE_SIGNAL_KEY,
+                    autoResubscribe: true,
+                    notifyButton: {
+                        enable: false,
+                    },
+                    welcomeNotification: {
+                        disable: true,
+                    },
+                    allowLocalhostAsSecureOrigin: true,
+                });
+            }
+        });
 
-    const tag = document.createElement('script');
-    tag.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
-    document.getElementsByTagName('head')[0].appendChild(tag);
+        const tag = document.createElement('script');
+        tag.src = 'https://cdn.onesignal.com/sdks/OneSignalSDK.js';
+        document.getElementsByTagName('head')[0].appendChild(tag);
+    }
 };
