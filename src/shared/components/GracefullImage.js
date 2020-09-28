d56d04c383f54ca6bce02db58f6365224258039e
Christopher Dengsø <work@cude.io>
2020-09-28 21:21:38 +0200

    use native lazy load

--- src/shared/components/GracefullImage.js
+++ src/shared/components/GracefullImage.js
@@ -1,74 +1,14 @@
-import React, { useCallback, useRef, useState } from 'react';
-import styled, { keyframes } from 'styled-components';
-import { useInView } from 'react-intersection-observer';
+import React from 'react';
+import styled from 'styled-components';
 
-export const keyframeFadeIn = keyframes`
-    from { opacity: 0; }
-    to   { opacity: 1; }
-
-`;
-const StyledImg = styled(({ animate, keyframeFadeIn, alt, ...rest }) => (
-    <img alt={alt} {...rest} />
-))`
-    opacity: 0;
-    animation: ${keyframeFadeIn} 400ms ease forwards;
-    animation-duration: ${({ animate }) => (animate ? '400ms' : '1ms')};
+const StyledImg = styled.img`
+    color: transparent;
 `;
 
-function useImageLoaded({ src }) {
-    const [loaded, setLoaded] = useState(false);
-    React.useEffect(() => {
-        if (src) {
-            const mainImage = new Image();
-            mainImage.onload = () => {
-                setLoaded(true);
-            };
-
-            mainImage.src = src;
-            return () => {
-                mainImage.onload = () => {};
-            };
-        }
-    }, [src]);
-
-    return loaded;
-}
-
-const GracefullImage = ({ src, style, placeholderStyle, alt, animate, lazyload, ...props }) => {
-    const [ref, inView] = useInView({
-        rootMargin: '200px',
-        triggerOnce: true,
-    });
-
-    let source = null;
-
-    if (lazyload) {
-        if (inView) {
-            source = src;
-        }
-    } else {
-        source = src;
-    }
-
-    const loaded = useImageLoaded({
-        src: source,
-    });
-
-    if (!loaded) {
-        return (
-            <div
-                ref={lazyload ? ref : null}
-                style={{
-                    backgroundColor: '#EFF2F5',
-                    ...style,
-                    ...placeholderStyle,
-                }}
-                {...props}
-            />
-        );
-    }
-
-    return <StyledImg src={src} style={style} alt={alt} animate={animate} {...props} />;
+const GracefullImage = ({ src, style, alt, animate, ...props }) => {
+    return (
+        <StyledImg src={src} style={style} loading="lazy" alt={alt} animate={animate} {...props} />
+    );
 };
 
 export const GracefullPicture = ({ children, style }) => {
