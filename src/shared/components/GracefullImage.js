fca4e1fe847b07d8ae4eb8744deaa6db47ce23db
Christopher Dengsø <work@cude.io>
2020-10-22 11:51:29 +0800

    empty image border

--- src/shared/components/GracefullImage.js
+++ src/shared/components/GracefullImage.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledImg = styled.img`
+    display: none;
     color: transparent;
+    &[src] {
+        display: block;
+    }
 `;
 
 const GracefullImage = ({ ...props }) => {
