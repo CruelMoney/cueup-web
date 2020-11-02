e7fe67f28236a13d95bb4df8705be14b4661c4b4
Christopher Dengsø <work@cude.io>
2020-11-02 12:54:21 +0800

    added social login to signup

--- src/shared/routes/Signup/components/Signup.js
+++ src/shared/routes/Signup/components/Signup.js
@@ -1,25 +1,73 @@
 import React from 'react';
 
+import styled from 'styled-components';
 import { Body } from 'components/Text';
 import { appRoutes } from 'constants/locales/appRoutes';
 
-import { Container } from 'components/Blocks';
+import { Container, Hr, SmartButton } from 'components/Blocks';
+import useSocialLogin from 'components/hooks/useSocialLogin';
+import { useServerContext } from 'components/hooks/useServerContext';
 import Footer from '../../../components/common/Footer';
-import SignUpForm from './SignUpForm';
+import fbLogo from '../../../assets/icons/fb.svg';
+import googleLogo from '../../../assets/icons/google.svg';
 import SignupHeader from './SignupHeader';
+import SignUpForm from './SignUpForm';
+
+const SocialButton = styled(SmartButton)`
+    min-width: 100%;
+    margin-bottom: 12px;
+    > span {
+        position: relative;
+        width: 100%;
+    }
+
+    > span:nth-child(2) {
+        position: absolute;
+        right: 1em;
+        width: auto;
+    }
+
+    img {
+        height: 20px;
+        left: 0px;
+        position: absolute;
+        top: 50%;
+        -webkit-transform: translateY(-50%);
+        transform: translateY(-50%);
+        width: 20px;
+    }
+`;
 
 const Signup = ({ translate }) => {
+    const { environment } = useServerContext();
+
+    const [onPressSocial, { socialLoading }] = useSocialLogin({
+        redirect: environment.WEBSITE_URL + '/complete-signup',
+    });
+
     return (
         <>
             <SignupHeader />
             <Container style={{ marginTop: 50, marginBottom: 80, maxWidth: 900 }}>
+                <SocialButton
+                    level="secondary"
+                    onClick={onPressSocial('facebook')}
+                    loading={socialLoading === 'facebook'}
+                >
+                    <img src={fbLogo} alt="facebook logo" />
+                    Continue with Facebook
+                </SocialButton>
+
+                <SocialButton
+                    level="secondary"
+                    onClick={onPressSocial('google')}
+                    loading={socialLoading === 'google'}
+                >
+                    <img src={googleLogo} alt="google logo" />
+                    Continue with Google
+                </SocialButton>
+                <Hr style={{ marginTop: 30 }} />
                 <div className="signup">
-                    <Body style={{ marginBottom: 50 }}>
-                        Fill out the form to create a profile. After you sign up, we will review
-                        your profile to ensure that you meet our requirements. If you don't get
-                        approved, we will let you know what changes you need to make. After you have
-                        been approved, you will be welcomed and get access to all features.
-                    </Body>
                     <SignUpForm translate={translate} />
                 </div>
             </Container>
