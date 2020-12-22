41f7edbdeca7b83a3f944711928134acab9ed4f4
Christopher Dengsø <work@cude.io>
2020-12-22 17:08:20 +0800

    removed old  code

--- src/shared/routes/Location/index.js
+++
@@ -1,6 +0,0 @@
-import loadable from '@loadable/component';
-const LazyLocation = loadable(() => import('./Location'));
-const LazyLocationsOverview = loadable(() => import('./LocationsOverview'));
-
-export { LazyLocationsOverview };
-export default LazyLocation;
