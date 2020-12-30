aad81bb9cfa7b5626bca6fba413ed10cced3d32f
Christopher Dengsø <work@cude.io>
2020-12-30 15:49:01 +0800

    styling

--- src/shared/routes/Home/components/TrustedBy.js
+++ src/shared/routes/Home/components/TrustedBy.js
@@ -10,66 +10,49 @@ import louboutin from '../assets/louboutin.svg';
 import politiken from '../assets/politiken.svg';
 import tivoli from '../assets/tivoli.svg';
 
-const Logos = styled(Row)`
+const Logos = styled.section`
+    display: grid;
     width: 100%;
     min-height: 200px;
+    grid-template-columns: repeat(6, 1fr);
+    column-gap: 4em;
+    margin-bottom: 60px;
+
     img {
         margin: auto;
-        width: 11%;
-        max-width: 200px;
-        max-height: 80px;
+        width: 100%;
+        height: 100%;
         opacity: 0.5;
         transition: opacity 500ms cubic-bezier(0.55, -0.01, 0.24, 1.01) 0s;
     }
     img:hover {
         opacity: 1;
     }
-    margin-bottom: 60px;
+    @media only screen and (max-width: 1000px) {
+        padding: 0 6em;
+        grid-template-columns: repeat(3, 1fr);
+        row-gap: 2em;
+        column-gap: 4em;
+        margin-top: 1.5em;
+    }
+    @media only screen and (max-width: 766px) {
+        padding: 0 1em;
+        grid-template-columns: repeat(2, 1fr);
+        row-gap: 2em;
+    }
 `;
 
 const TrustedBy = () => {
     return (
         <Col middle style={{ width: '100%' }}>
-            <TextAccent>Trusted by</TextAccent>
+            <TextAccent style={{ color: '#4d6480', opacity: 0.5 }}>Trusted by</TextAccent>
 
             <Logos>
-                <GracefullImage
-                    src={microsoft}
-                    // style={{
-                    //     width: '200px',
-                    //     height: '60px',
-                    // }}
-                />
-                <GracefullImage
-                    src={hardRock}
-                    // style={{
-                    //     width: '150px',
-                    //     maxHeight: '150px',
-                    // }}
-                />
-                <GracefullImage
-                    src={hakkasan}
-                    style={
-                        {
-                            // width: '170px',
-                            // height: '70px',
-                        }
-                    }
-                />
-                <GracefullImage
-                    src={politiken}
-                    // style={{
-                    //     width: '200px',
-                    //     maxHeight: '100px',
-                    // }}
-                />
-                <GracefullImage
-                    src={tivoli}
-                    // style={{
-                    //     width: '150px',
-                    //     maxHeight: '100px',
-                    // }}
-                />
+                <GracefullImage src={microsoft} />
+                <GracefullImage src={hardRock} />
+                <GracefullImage src={hakkasan} />
+                <GracefullImage src={politiken} />
+                <GracefullImage src={tivoli} />
                 <GracefullImage src={louboutin} />
             </Logos>
         </Col>
